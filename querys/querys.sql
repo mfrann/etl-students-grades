2a1845a11703c70da1ce1e9fe1af35502c799ffa
Fran <mfranfc@gmail.com>
2026-01-02 15:08:42 -0500

    feat: add querys

--- querys/querys.sql
+++ querys/querys.sql
@@ -1,13 +1,20 @@
+--QUERYS BASICAS
+
+-- Contar los estudiantes
+SELECT COUNT(DISTINCT student_id) as total_estudiantes
+FROM students_grades;
+
 -- Seleccionar promedio de alumnos
 SELECT name, average 
 FROM students_grades; 
 
-
 -- Seleccionar alumnos aprobados
-SELECT DISTINCT name, average 
+SELECT DISTINCT name, average, status 
 FROM students_grades
 WHERE status='PASS';
 
--- Contar los estudiantes
-SELECT COUNT(DISTINCT student_id) as total_estudiantes
+-- Seleccionar estudiantes desaprobados
+SELECT DISTINCT name, average, status
 FROM students_grades
+WHERE status='NO PASS';
+
